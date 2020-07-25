f7e70d9d7208f53c33c03edfa5e6b1f8b3eba1f7
Valentyn Lukashev <valikallstars@gmail.com>
2020-07-25 18:58:36 +0300

    commit

--- js/slider.js
+++ js/slider.js
@@ -1,9 +1,13 @@
 const slider = document.querySelector('.swiper-container');
-
+const sliderDetroitNum = document.querySelector('.swiper-container_detroit-in-num');
 let mySwiper = new Swiper(slider, {
 	slidesPerView: 2,
 	spaceBetween: 20,
 	loop: true,
+	speed: 900,
+	autoplay: {
+		delay: 10000,
+	},
 	pagination: {
 		el: '.swiper-pagination',
 		clickable: true,
@@ -13,3 +17,73 @@ let mySwiper = new Swiper(slider, {
 		prevEl: '.swiper-button-prev',
 	},
 })
+var mySwiperInNum = new Swiper('.container_detroit-in-num', {
+	// Optional parameters
+	direction: 'horizontal',
+	loop: true,
+	slidesPerView: 1,
+	speed: 900,
+	spaceBetween: 1,
+	autoplay: {
+		delay: 10000,
+	},
+	// If we need pagination
+	pagination: {
+		el: '.pagination_detroit-in-num',
+		clickable: true,
+	},
+
+	// Navigation arrows
+	navigation: {
+		nextEl: '.button-next-in-num',
+		prevEl: '.button-prev-in-num',
+	},
+
+})
+// /* <!-- window 7 --> */
+
+$(".tab_item").not(":first").hide();
+$(".wrapper .tab").click(function () {
+	$(".wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+	$(".tab_item").hide().eq($(this).index()).fadeIn()
+}).eq(0).addClass("active");
+
+// window 8
+var mySwiperReviews = new Swiper('.container_reviews', {
+	// Optional parameters
+	direction: 'horizontal',
+	loop: true,
+	slidesPerView: 1,
+	speed: 900,
+	spaceBetween: 1,
+	autoplay: {
+		delay: 10000,
+	},
+	// If we need pagination
+	pagination: {
+		el: '.pagination_reviews',
+		clickable: true,
+	},
+
+	// Navigation arrows
+	navigation: {
+		nextEl: '.button-next-reviews',
+		prevEl: '.button-prev-reviews',
+	},
+
+})
+/* <!-- аккардеон --> */
+
+// document.querySelectorAll('.accordeon-item__trigger').forEach((item) =>
+// 	item.addEventListener('click', () => {
+// 		const parent = item.parentNode;
+
+// 		if (parent.classList.contains('accordeon-item--active')) {
+// 			parent.classList.remove('accordeon-item--active');
+// 		} else {
+// 			document.querySelectorAll('.accordeon-item')
+// 				.forEach((child) => child.classList.remove('accordeon-item--active'))
+// 			parent.classList.toggle('accordeon-item--active');
+// 		}
+// 	})
+// )
